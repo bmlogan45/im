9010592be415cdf7a4f12544d522a7569dd2955a
Cindy Hom <ch991@georgetown.edu>
2016-03-18 15:46:46 -0400

    List version commit

---
+++ ChatServer.java
@@ -0,0 +1,91 @@
+import java.net.*;
+import java.util.ArrayList;
+import java.io.*;
+
+public class ChatServer implements Runnable
+{  
+   private ArrayList<ChatServerThread> clients = new ArrayList<ChatServerThread>();
+   private ServerSocket server = null;
+   private Thread       thread = null;
+   private int clientCount = 0;
+
+   public ChatServer(int port)
+   {  try
+      {  System.out.println("Binding to port " + port + ", please wait  ...");
+         server = new ServerSocket(port);  
+         System.out.println("Server started: " + server);
+         start(); }
+      catch(IOException ioe)
+      {  System.out.println("Can not bind to port " + port + ": " + ioe.getMessage()); }
+   }
+   public void run()
+   {  while (thread != null)
+      {  try
+         {  System.out.println("Waiting for a client ..."); 
+            addThread(server.accept()); }
+         catch(IOException ioe)
+         {  System.out.println("Server accept error: " + ioe); stop(); }
+      }
+   }
+   public void start()  { 
+	   if (thread == null)
+	      {  thread = new Thread(this); 
+	         thread.start();
+	      } 
+	   }
+   public void stop()   {
+	   if (thread != null)
+	      {  thread.stop(); 
+	         thread = null;
+	      }
+
+	   }
+   private int findClient(int ID)
+   {  for (int i = 0; i < clientCount; i++)
+         if (clients.get(i).getID() == ID)
+            return i;
+      return -1;
+   }
+   public synchronized void handle(int ID, String input)
+   {  if (input.equals(".bye"))
+      {  clients.get(findClient(ID)).send(".bye");
+         remove(ID); }
+      else
+         for (int i = 0; i < clientCount; i++)
+            clients.get(i).send(ID + ": " + input);   
+   }
+   public synchronized void remove(int ID)
+   {  int pos = findClient(ID);
+      if (pos >= 0)
+      {  ChatServerThread toTerminate = clients.get(pos);
+         System.out.println("Removing client thread " + ID + " at " + pos);
+         if (pos < clientCount-1)
+            for (int i = pos+1; i < clientCount; i++)
+            	clients.set((i-1), clients.get(i));
+         clientCount--;
+         try
+         {  toTerminate.close(); }
+         catch(IOException ioe)
+         {  System.out.println("Error closing thread: " + ioe); }
+         toTerminate.stop(); }
+   }
+   private void addThread(Socket socket)
+   {  if (clientCount < clients.size())
+      {  System.out.println("Client accepted: " + socket);
+      	 clients.set(clientCount, new ChatServerThread(this, socket));
+         try
+         {  
+        	clients.get(clientCount).open();
+        	clients.get(clientCount).start(); 
+            clientCount++; }
+         catch(IOException ioe)
+         {  System.out.println("Error opening thread: " + ioe); } }
+      else
+         System.out.println("Client refused: maximum " + clients.size() + " reached.");
+   }
+   public static void main(String args[]) { 
+	   ChatServer server = new ChatServer(10000);  
+	   ChatClient proxy = new ChatClient("localhost", 10000); 
+   
+    }
+}
